454a3ae765236fd80ad10ab8d8522cd6fd05afc2
Gambled23 <ipog71@gmail.com>
2023-03-22 14:19:58 -0600

    like si tu tmbn

---
+++ semana 6/index.js
@@ -0,0 +1,15 @@
+function prueba() {
+    //jaja hola
+    let entrada1 = document.getElementById('entrada1').value
+    let entrada2 = document.getElementById('entrada2').value
+    let entrada3 = document.getElementById('entrada3').value
+    let entrada4 = document.getElementById('entrada4').value
+    let entrada5 = document.getElementById('entrada5').value
+    let entrada6 = document.getElementById('entrada6').value
+    let entrada7 = document.getElementById('entrada7').value
+    let entrada8 = document.getElementById('entrada8').value
+    let entrada9 = document.getElementById('entrada9').value
+    var arregloInputs = [entrada1,entrada2,entrada3,entrada4,entrada5,entrada6,entrada7,entrada8,entrada9]
+    
+    
+}
